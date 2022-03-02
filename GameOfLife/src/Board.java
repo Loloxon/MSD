9ea881559017f14541ed75459ae2278d916273ed
loloxon <nikodemko@interia.pl>
2022-03-02 23:15:59 +0100

    rain

--- GameOfLife/src/Board.java
+++ GameOfLife/src/Board.java
@@ -1,3 +1,4 @@
+
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Insets;
@@ -14,180 +15,186 @@ import javax.swing.event.MouseInputListener;
  */
 
 public class Board extends JComponent implements MouseInputListener, ComponentListener {
-	private static final long serialVersionUID = 1L;
-	private Point[][] points;
-	private int size = 14;
+    private static final long serialVersionUID = 1L;
+    private Point[][] points;
+    private int size = 14;
 
-	public Board(int length, int height) {
-		addMouseListener(this);
-		addComponentListener(this);
-		addMouseMotionListener(this);
-		setBackground(Color.WHITE);
-		setOpaque(true);
-	}
+    public Board(int length, int height) {
+        addMouseListener(this);
+        addComponentListener(this);
+        addMouseMotionListener(this);
+        setBackground(Color.WHITE);
+        setOpaque(true);
+    }
 
-	// single iteration
-	public void iteration() {
-		for (int x = 0; x < points.length; ++x)
-			for (int y = 0; y < points[x].length; ++y)
-				points[x][y].calculateNewState();
+    // single iteration
+    public void iteration() {
+//		for (int x = 0; x < points.length; ++x)
+//			for (int y = 0; y < points[x].length; ++y)
+//				points[x][y].calculateNewState();
 
-		for (int x = 0; x < points.length; ++x)
-			for (int y = 0; y < points[x].length; ++y)
-				points[x][y].changeState();
-		this.repaint();
+        for (int x = 0; x < points.length; ++x)
+            for (int y = 0; y < points[x].length; ++y)
+                points[x][y].changeState();
+        this.repaint();
 
-//        for (int x=0;x<points.length;++x){
-//            points[x][0].drop();
-//        }
-//
-//        for (int x = 0; x < points.length; ++x)
-//            for (int y = 0; y < points[x].length; ++y) {
-//                if (points[x][y].getState() > 0)
-//                    points[x][y].setNextState(points[x][y].getState() - 1);
-//                if (points[x][y].getState() == 0 && points[x][y].sumAliveNeighbors()==1)
-//                    points[x][y].setNextState(6);
-//            }
-	}
+        for (int x=1;x<points.length-1;++x){
+            points[x][1].drop();
+        }
 
-	// clearing board
-	public void clear() {
-		for (int x = 0; x < points.length; ++x)
-			for (int y = 0; y < points[x].length; ++y) {
-				points[x][y].setState(0);
-			}
-		this.repaint();
-	}
+        for (int x = 1; x < points.length-1; ++x)
+            for (int y = 1; y < points[x].length-1; ++y) {
+                if (points[x][y].getState() > 0) {
+                    points[x][y].setNextState(points[x][y].getState() - 1);
+                    if (points[x][y].sumAliveNeighbors() == 0 && y<points[x].length-2)
+                        points[x][y + 1].setNextState(6);
+                }
+            }
+    }
 
-	private void initialize(int length, int height) {
-		points = new Point[length][height];
+    // clearing board
+    public void clear() {
+        for (int x = 0; x < points.length; ++x)
+            for (int y = 0; y < points[x].length; ++y) {
+                points[x][y].setState(0);
+            }
+        this.repaint();
+    }
 
-		for (int x = 0; x < points.length; ++x)
-			for (int y = 0; y < points[x].length; ++y)
-				points[x][y] = new Point();
+    private void initialize(int length, int height) {
+        points = new Point[length][height];
 
-		for (int x = 1; x < points.length-1; ++x) {
-			for (int y = 1; y < points[x].length-1; ++y) {
-                points[x][y].addNeighbor(points[x-1][y-1]);
-                points[x][y].addNeighbor(points[x-1][y]);
-                points[x][y].addNeighbor(points[x-1][y+1]);
-                points[x][y].addNeighbor(points[x][y-1]);
+        for (int x = 0; x < points.length; ++x)
+            for (int y = 0; y < points[x].length; ++y)
+                points[x][y] = new Point();
+
+        for (int x = 1; x < points.length-1; ++x) {
+            for (int y = 1; y < points[x].length-1; ++y) {
+//                points[x][y].addNeighbor(points[x-1][y-1]);
+//                points[x][y].addNeighbor(points[x-1][y]);
+//                points[x][y].addNeighbor(points[x-1][y+1]);
+//                points[x][y].addNeighbor(points[x][y-1]);
                 points[x][y].addNeighbor(points[x][y+1]);
-                points[x][y].addNeighbor(points[x+1][y-1]);
-                points[x][y].addNeighbor(points[x+1][y]);
-                points[x][y].addNeighbor(points[x+1][y+1]);
-				//TODO: initialize the neighborhood of points[x][y] cell
-			}
-		}
-	}
+//                points[x][y].addNeighbor(points[x+1][y-1]);
+//                points[x][y].addNeighbor(points[x+1][y]);
+//                points[x][y].addNeighbor(points[x+1][y+1]);
+                //TODO: initialize the neighborhood of points[x][y] cell
+            }
+        }
+    }
 
-	//paint background and separators between cells
-	protected void paintComponent(Graphics g) {
-		if (isOpaque()) {
-			g.setColor(getBackground());
-			g.fillRect(0, 0, this.getWidth(), this.getHeight());
-		}
-		g.setColor(Color.GRAY);
-		drawNetting(g, size);
-	}
+    //paint background and separators between cells
+    protected void paintComponent(Graphics g) {
+        if (isOpaque()) {
+            g.setColor(getBackground());
+            g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        }
+        g.setColor(Color.GRAY);
+        drawNetting(g, size);
+    }
 
-	// draws the background netting
-	private void drawNetting(Graphics g, int gridSpace) {
-		Insets insets = getInsets();
-		int firstX = insets.left;
-		int firstY = insets.top;
-		int lastX = this.getWidth() - insets.right;
-		int lastY = this.getHeight() - insets.bottom;
+    // draws the background netting
+    private void drawNetting(Graphics g, int gridSpace) {
+        Insets insets = getInsets();
+        int firstX = insets.left;
+        int firstY = insets.top;
+        int lastX = this.getWidth() - insets.right;
+        int lastY = this.getHeight() - insets.bottom;
 
-		int x = firstX;
-		while (x < lastX) {
-			g.drawLine(x, firstY, x, lastY);
-			x += gridSpace;
-		}
+        int x = firstX;
+        while (x < lastX) {
+            g.drawLine(x, firstY, x, lastY);
+            x += gridSpace;
+        }
 
-		int y = firstY;
-		while (y < lastY) {
-			g.drawLine(firstX, y, lastX, y);
-			y += gridSpace;
-		}
+        int y = firstY;
+        while (y < lastY) {
+            g.drawLine(firstX, y, lastX, y);
+            y += gridSpace;
+        }
 
-		for (x = 0; x < points.length; ++x) {
-			for (y = 0; y < points[x].length; ++y) {
-				if (points[x][y].getState() != 0) {
-//                    g.setColor(new Color(0.0f, 0.0f, 1.0f, 0.65f));
-					switch (points[x][y].getState()) {
-					case 1:
-						g.setColor(new Color(0x0000ff));
-						break;
-					case 2:
-						g.setColor(new Color(0x00ff00));
-						break;
-					case 3:
-						g.setColor(new Color(0xff0000));
-						break;
-					case 4:
-						g.setColor(new Color(0x000000));
-						break;
-					case 5:
-						g.setColor(new Color(0x444444));
-						break;
-					case 6:
-						g.setColor(new Color(0xffffff));
-						break;
-					}
-					g.fillRect((x * size) + 1, (y * size) + 1, (size - 1), (size - 1));
-				}
-			}
-		}
+        for (x = 0; x < points.length; ++x) {
+            for (y = 0; y < points[x].length; ++y) {
+                if (points[x][y].getState() != 0) {
+                    switch (points[x][y].getState()) {
+                        case 1:
+//						g.setColor(new Color(0x0000ff));
+                            g.setColor(new Color(0.0f, 0.0f, 1.0f, 0.17f));
+                            break;
+                        case 2:
+//						g.setColor(new Color(0x00ff00));
+                            g.setColor(new Color(0.0f, 0.0f, 1.0f, 0.33f));
+                            break;
+                        case 3:
+//						g.setColor(new Color(0xff0000));
+                            g.setColor(new Color(0.0f, 0.0f, 1.0f, 0.5f));
+                            break;
+                        case 4:
+//						g.setColor(new Color(0x000000));
+                            g.setColor(new Color(0.0f, 0.0f, 1.0f, 0.67f));
+                            break;
+                        case 5:
+//						g.setColor(new Color(0x444444));
+                            g.setColor(new Color(0.0f, 0.0f, 1.0f, 0.83f));
+                            break;
+                        case 6:
+//						g.setColor(new Color(0xffffff));
+                            g.setColor(new Color(0.0f, 0.0f, 1.0f, 1.0f));
+                            break;
+                    }
+                    g.fillRect((x * size) + 1, (y * size) + 1, (size - 1), (size - 1));
+                }
+            }
+        }
 
-	}
+    }
 
-	public void mouseClicked(MouseEvent e) {
-		int x = e.getX() / size;
-		int y = e.getY() / size;
-		if ((x < points.length) && (x > 0) && (y < points[x].length) && (y > 0)) {
-			points[x][y].clicked();
-			this.repaint();
-		}
-	}
+    public void mouseClicked(MouseEvent e) {
+        int x = e.getX() / size;
+        int y = e.getY() / size;
+        if ((x < points.length) && (x > 0) && (y < points[x].length) && (y > 0)) {
+            points[x][y].clicked();
+            this.repaint();
+        }
+    }
 
-	public void componentResized(ComponentEvent e) {
-		int dlugosc = (this.getWidth() / size) + 1;
-		int wysokosc = (this.getHeight() / size) + 1;
-		initialize(dlugosc, wysokosc);
-	}
+    public void componentResized(ComponentEvent e) {
+        int dlugosc = (this.getWidth() / size) + 1;
+        int wysokosc = (this.getHeight() / size) + 1;
+        initialize(dlugosc, wysokosc);
+    }
 
-	public void mouseDragged(MouseEvent e) {
-		int x = e.getX() / size;
-		int y = e.getY() / size;
-		if ((x < points.length) && (x > 0) && (y < points[x].length) && (y > 0)) {
-			points[x][y].setState(1);
-			this.repaint();
-		}
-	}
+    public void mouseDragged(MouseEvent e) {
+        int x = e.getX() / size;
+        int y = e.getY() / size;
+        if ((x < points.length) && (x > 0) && (y < points[x].length) && (y > 0)) {
+            points[x][y].setState(1);
+            this.repaint();
+        }
+    }
 
-	public void mouseExited(MouseEvent e) {
-	}
+    public void mouseExited(MouseEvent e) {
+    }
 
-	public void mouseEntered(MouseEvent e) {
-	}
+    public void mouseEntered(MouseEvent e) {
+    }
 
-	public void componentShown(ComponentEvent e) {
-	}
+    public void componentShown(ComponentEvent e) {
+    }
 
-	public void componentMoved(ComponentEvent e) {
-	}
+    public void componentMoved(ComponentEvent e) {
+    }
 
-	public void mouseReleased(MouseEvent e) {
-	}
+    public void mouseReleased(MouseEvent e) {
+    }
 
-	public void mouseMoved(MouseEvent e) {
-	}
+    public void mouseMoved(MouseEvent e) {
+    }
 
-	public void componentHidden(ComponentEvent e) {
-	}
+    public void componentHidden(ComponentEvent e) {
+    }
 
-	public void mousePressed(MouseEvent e) {
-	}
+    public void mousePressed(MouseEvent e) {
+    }
 
 }
--- GameOfLife/src/Point.java
+++ GameOfLife/src/Point.java
@@ -2,63 +2,65 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Point {
-	private ArrayList<Point> neighbors;
-	private int currentState;
-	private int nextState;
-	private int numStates = 6;
-	
-	public Point() {
-		currentState = 0;
-		nextState = 0;
-		neighbors = new ArrayList<Point>();
-	}
+    private ArrayList<Point> neighbors;
+    private int currentState;
+    private int nextState;
+    private int numStates = 6;
 
-	public void clicked() {
-		currentState=(++currentState)%numStates;	
-	}
-	
-	public int getState() {
-		return currentState;
-	}
+    public Point() {
+        currentState = 0;
+        nextState = 0;
+        neighbors = new ArrayList<Point>();
+    }
 
-	public void setState(int s) {
-		currentState = s;
-	}
+    public void clicked() {
+        currentState=(++currentState)%numStates;
+    }
 
-	public void calculateNewState() {
+    public int getState() {
+        return currentState;
+    }
+
+    public void setState(int s) {
+        currentState = s;
+    }
+
+    public void setNextState(int s){nextState = s;}
+
+    public void calculateNewState() {
         if(this.currentState==1)
-            if(this.sumAliveNeighbors()>=2 && this.sumAliveNeighbors()<=5)
-                this.nextState = 1;
-            else
-                this.nextState = 0;
-        if(this.currentState==0)
             if(this.sumAliveNeighbors()>=4 && this.sumAliveNeighbors()<=8)
                 this.nextState = 1;
             else
                 this.nextState = 0;
-		//TODO: insert logic which updates according to currentState and number of active neighbors
-	}
+        if(this.currentState==0)
+            if(this.sumAliveNeighbors()>=3 && this.sumAliveNeighbors()<=8)
+                this.nextState = 1;
+            else
+                this.nextState = 0;
+        //TODO: insert logic which updates according to currentState and number of active neighbors
+    }
 
-	public void changeState() {
-		currentState = nextState;
-	}
-	
-	public void addNeighbor(Point nei) {
-		neighbors.add(nei);
-	}
+    public void changeState() {
+        currentState = nextState;
+    }
+
+    public void addNeighbor(Point nei) {
+        neighbors.add(nei);
+    }
 
     public int sumAliveNeighbors(){
         int tmp=0;
         for (Point neighbor : neighbors)
-            if (neighbor.getState() == 1)
+            if (neighbor.getState() >= 1)
                 tmp += 1;
         return tmp;
     }
-	//TODO: write method counting all active neighbors of THIS point
+    //TODO: write method counting all active neighbors of THIS point
 
-//    public void drop(){
-//        Random random = new Random();
-//        if(random.nextInt(100)<5)
-//            this.currentState = 6;
-//    }
+    public void drop(){
+        Random random = new Random();
+        if(random.nextInt(100)<5)
+            this.currentState = 6;
+    }
 }
