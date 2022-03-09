4bcc577a71bedebf2d2022f398d05eb2e68e7e79
loloxon <nikodemko@interia.pl>
2022-03-09 17:19:56 +0100

    lab2

--- Lab 2/SoundWave/src/Point.java
+++ Lab 2/SoundWave/src/Point.java
@@ -10,8 +10,14 @@ public class Point {
 	public float sVel;
 	public float pressure;
 
+    public int sinInput;
+    
+    public static Integer []types ={0,1,2};
+    int type;
+
 	public Point() {
 		clear();
+        type = 0;
 	}
 
 	public void clicked() {
@@ -20,14 +26,46 @@ public class Point {
 	
 	public void clear() {
 		// TODO: clear velocity and pressure
-	}
+        nVel = 0;
+        eVel = 0;
+        wVel = 0;
+        sVel = 0;
+	    pressure = 0;
+    }
 
 	public void updateVelocity() {
 		// TODO: velocity update
-	}
+        if(type==0) {
+            nVel = nVel - (nNeighbor.pressure - pressure);
+            eVel = eVel - (eNeighbor.pressure - pressure);
+            wVel = wVel - (wNeighbor.pressure - pressure);
+            sVel = sVel - (sNeighbor.pressure - pressure);
+        }
+//        float nVeltmp = nVel - (nNeighbor.pressure - pressure);
+//        float eVeltmp = eVel - (eNeighbor.pressure - pressure);
+//        float wVeltmp = wVel - (wNeighbor.pressure - pressure);
+//        float sVeltmp = sVel - (sNeighbor.pressure - pressure);
+//        nVel = nVeltmp;
+//        eVel = eVeltmp;
+//        sVel = sVeltmp;
+//        wVel = wVeltmp;
+    }
 
 	public void updatePresure() {
 		// TODO: pressure update
+        if(type==0) {
+            float summedVel = 0;
+//        summedVel += nVel - (nNeighbor.pressure - pressure);
+//        summedVel += eVel - (eNeighbor.pressure - pressure);
+//        summedVel += wVel - (wNeighbor.pressure - pressure);
+//        summedVel += sVel - (sNeighbor.pressure - pressure);
+            summedVel = nVel + eVel + sVel + wVel;
+            pressure = pressure - (summedVel) / 2;
+        }
+        else if(type==2){
+            double radians = Math.toRadians(sinInput);
+            pressure = (float) (Math.sin(radians));
+        }
 	}
 
 	public float getPressure() {
